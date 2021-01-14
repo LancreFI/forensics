e45f42ba5213fbf1c330207ed5e606cd916d6521
LancreFI <50398030+LancreFI@users.noreply.github.com>
2021-01-14 16:43:08 +0200

    Update extback.sh

--- extback.sh
+++ extback.sh
@@ -5,28 +5,24 @@ echo " "
 
 if [ -e "$1" ]
 then
-        #if [ ! -e "$1"Â ]
-        #then
-        #       echo "File $1 not found!"
-        #else
-                if [ -d "$1_extracted" ]
-                then
-                        echo "Looks like you might have already extracted this backup to:"
-                        echo "  $1_extracted"
-                        echo "Move or rename the old extracted folder first!"
-                else
-                        echo "Extracting $1..."
-                        mkdir "$1_extracted"
-                        cd "$1_extracted"
-                        cp ../"$1" .
-                        ##THE BACKUP FILE IS A TAR FILE, MISSING THE HEADER, SO WE ADD IT,
-                        ##SKIP THE ANDROID'S HEADER AND PIPE TO TAR FOR EXTRACTION
-                        ( printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" ; tail -c +25 "$1" )|tar xvfz - 2>/dev/null
-                        echo " "
-                        echo "Extraction completed! The resulting content can be found"
-                        echo "in $1_extracted folder."
-                        rm "$1"
-                fi
+        if [ -d "$1_extracted" ]
+        then
+                echo "Looks like you might have already extracted this backup to:"
+                echo "  $1_extracted"
+                echo "Move or rename the old extracted folder first!"
+        else
+                echo "Extracting $1..."
+                mkdir "$1_extracted"
+                cd "$1_extracted"
+                cp ../"$1" .
+                ##THE BACKUP FILE IS A TAR FILE, MISSING THE HEADER, SO WE ADD IT,
+                ##SKIP THE ANDROID'S HEADER AND PIPE TO TAR FOR EXTRACTION
+                ( printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" ; tail -c +25 "$1" )|tar xvfz - 2>/dev/null
+                echo " "
+                echo "Extraction completed! The resulting content can be found"
+                echo "in $1_extracted folder."
+                rm "$1"
+        fi
 else
         if [[ ! -z "$1" ]]
         then
